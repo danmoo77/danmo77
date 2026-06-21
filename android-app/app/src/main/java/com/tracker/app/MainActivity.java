7b6f8324979243b8155382554104748a990511a4
tracker-user <tracker@example.com>
2026-06-21 23:26:03 +0800

    Add Android WebView app + CI auto-build APK

---
+++ android-app/app/src/main/java/com/tracker/app/MainActivity.java
@@ -0,0 +1,36 @@
+package com.tracker.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://danmoo77.github.io/danmo77/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content);
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
